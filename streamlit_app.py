9edbbe7ed236662078a4bc1e2159f47f6aa340a0
Nandini Bavana <105676665+NandiniSnowflake@users.noreply.github.com>
2022-05-20 11:17:59 +1200

    Update streamlit_app.py
    Multi-Select widget moved to another location

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("pick some Fruits: ", list(my_fruit_list.index))
+
 streamlit.title("My Mom's New Healthy Dinner")
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -12,4 +12,5 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.multiselect("pick some Fruits: ", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
