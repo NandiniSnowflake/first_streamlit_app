c009dad9ca4552a8943c8e508fcb3cacc0db8e64
Nandini Bavana <105676665+NandiniSnowflake@users.noreply.github.com>
2022-05-20 11:16:36 +1200

    Update streamlit_app.py
    A Multi-select widget added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 
 import streamlit
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("pick some Fruits: ", list(my_fruit_list.index))
 streamlit.title("My Mom's New Healthy Dinner")
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
