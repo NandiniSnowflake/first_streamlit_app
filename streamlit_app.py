26abeb9a799e488f545a9ed7d28aca5e2849893b
Nandini Bavana <105676665+NandiniSnowflake@users.noreply.github.com>
2022-05-30 06:46:50 +1200

    Update streamlit_app.py
    Added requests library and import watermelon fro fruityvise

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 
 import streamlit
 import pandas
+import requests
 
+fruityvise_response = requests.get("https://www.fruityvice.com//api/fruit/watermelon")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.title("My Mom's New Healthy Dinner")
@@ -10,6 +12,7 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text(fruityvise_response)
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected = streamlit.multiselect("pick some Fruits: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
